9ecf1518a234cae0e415a3f97f7b2f38e5f66f7b
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-02 20:11:16 +0300

    Render ContactForm with props

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -25,7 +25,13 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name} setName={setName}
+          phone={phone} setPhone={setPhone}
+          email={email} setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
