a179f8976fe8aef9dd83f64aacbfc03ed6ebc1ac
Pavlo Kvas <kvaspasha@gmail.com>
2024-07-02 22:39:19 +0300

    Render a form with 3 inputs

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,21 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name: <input type='text' value={name}
+          onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Phone: <input type='tel' value={phone}
+          onChange={(e) => setPhone(e.target.value)} />
+      </label>
+      <label>
+        Email: <input type='email' value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <button type='submit'>Submit</button>
+    </form>
   );
 };
 
